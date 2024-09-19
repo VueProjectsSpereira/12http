f914484cfafc3eb4c251fbac8b3ebb8ff3481480
Saul Pereira <saul_pereira@hotmail.com>
2024-09-19 16:48:22 -0600

    164. Handling Technical / Browser-side Errors
    164. Handling Technical / Browser-side Errors

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -8,11 +8,12 @@
         >
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
       <p v-else-if="!isLoading && (!results || results.length === 0)">
         No stored experiences results found. Start adding some survey results
         first.
       </p>
-      <ul v-else-if="!isLoading && results && results.length > 0">
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -35,11 +36,13 @@ export default {
     return {
       results: [],
       isLoading: false,
+      error: null,
     };
   },
   methods: {
     loadExperiences() {
       this.isLoading = true;
+      this.error = null;
       fetch('https://vue-http-e055e-default-rtdb.firebaseio.com/surveys.json')
         .then((response) => {
           if (response.ok) {
@@ -58,6 +61,11 @@ export default {
             });
           }
           this.results = results;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = 'Failed to fetch data - please try again later.';
         });
     },
   },
